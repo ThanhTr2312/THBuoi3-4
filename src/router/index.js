2d766171c62b3a1fd6912b7bda48357d9e805fc3
Thanh <thanhb1906761@student.ctu.edu.vn>
2021-10-23 18:50:52 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import { createWebHistory, createRouter } from "vue-router";
-const routes = [{    
-    path: "/",
-    alias: "/contactbook",
-    name: "ContactBook",
-    component: () =>
-        import ("../views/ContactBook"),
-}, ];
-const router = createRouter({  
+const routes = [{
+        path: "/",
+        alias: "/contactbook",
+        name: "ContactBook",
+        component: () =>
+            import ("../views/ContactBook"),
+    },
+    {
+        path: "/contacts/:id",
+        name: "EditContact",
+        component: () =>
+            import ("../views/ContactEdit.vue"),
+    },
+];
+
+const router = createRouter({
     history: createWebHistory(),
     routes,
 });
